9f11e56b7744cfbaeaabc56d75838bbf2734e258
Abhay2412 <abhaykhosla0@gmail.com>
2023-02-03 11:56:09 -0700

    Create a first screen of the application

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Testing to see if the setup is performed correctly')
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+function createMainWindow() {
+    const mainWindowDisplay = new BrowserWindow({
+        title: 'Image Resizer',
+        width: 500,
+        height: 600
+    });
+
+    mainWindowDisplay.loadFile(path.join(__dirname, './client/index.html'));
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+});
